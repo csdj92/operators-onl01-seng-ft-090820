0ec935e6e64322030927cd2791e8a158dab80c91
CJ Davis <csdj92@gmail.com>
2020-09-09 18:22:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,7 +10,11 @@ end
 
 
 def not_safe?(speed)
+<<<<<<< HEAD
 speed < 40 || speed > 60 ? true: false
+=======
+40<speed || 60 <speed ? true:false
+>>>>>>> dfc8fe4dcf0bb9b31bbc18c78d749509cbc701d9
 end
 	
 
